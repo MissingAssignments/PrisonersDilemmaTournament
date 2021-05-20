a2f47f566c6b2c11e28c6e24760d4fc4cd9ac1f0
LikeToAccess <37591675+LikeToAccess@users.noreply.github.com>
2021-05-20 15:29:28 -0500

    bhhiubuibiibu

--- code/exampleStrats/tit4tatN.py
+++ code/exampleStrats/tit4tatN.py
@@ -1,12 +1,22 @@
+import random
+
 def strategy(history, memory):
 	if not memory:
 		memory = 0
 	choice = 1
-	if (history.shape[1] >= 1 and history[1,-1] == 0): # Choose to defect if and only if the opponent just defected.
+	if (history.shape[1] >= 1 and history[1,-1] == 0):# or memory > 2: # Choose to defect if and only if the opponent just defected.
+		memory += 1
 		choice = 0
-	if memory % 25 == 0:  # 10 < 9 > 8
-		choice = 1
-	return choice, memory + 1
+		# print(history[1,-1] == 0)
+	false_count = 0
+	for ans in history[1,-1]:
+		if not ans:
+			false_count += 1
+	if false_count > 3:
+		choice = 0
+		# if (memory+1) % 3 == 0:  # 10 < 9 > 8
+		# 	choice = 1
+	return choice, memory
 
 '''
 SCORE HISTORY
