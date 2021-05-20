61ce1d73963e4a95d2279cc41496f5a23d5fc7ed
LikeToAccess <37591675+LikeToAccess@users.noreply.github.com>
2021-05-20 12:23:11 -0500

    Stuff

--- code/exampleStrats/DEUS_STRAT.py
+++ code/exampleStrats/DEUS_STRAT.py
@@ -1,7 +1,24 @@
+# def strategy(history, memory):
+# 	choice = 0  # 1=C, 0=D
+# 	if not memory:
+# 		memory = 0
+# 	if memory % 6 == 0:
+# 		choice = 1
+# 	return choice, memory + 1
+
+
 def strategy(history, memory):
-	choice = 0  # 1=C, 0=D
+	wronged = False
 	if not memory:
-		memory = 0
-	if memory % 6 == 0:
-		choice = 1
-	return choice, memory + 1
+		memory = (False, 0)
+	if memory[0] is not None and memory[0]: # Has memory that it was already wronged.
+		wronged = True
+
+	else: # Has not been wronged yet, historically.
+		if history.shape[1] >= 1 and history[1,-1] == 0: # Just got wronged.
+			wronged = True
+
+	if wronged:
+		return 0, (True, memory[1]+1)
+	else:
+		return 1, (False, memory[1]+1)
