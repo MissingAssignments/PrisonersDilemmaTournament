ca8219411e578a3b7f18ef2b1880f661aa1e4736
LikeToAccess <37591675+LikeToAccess@users.noreply.github.com>
2021-05-20 14:05:13 -0500

    vetgtve

---
+++ code/exampleStrats/tit4tat9.py
@@ -0,0 +1,19 @@
+def strategy(history, memory):
+	if not memory:
+		memory = 0
+	choice = 1
+	if (history.shape[1] >= 1 and history[1,-1] == 0): # Choose to defect if and only if the opponent just defected.
+		choice = 0
+	if memory % 9 == 0:  # 10
+		choice = 1
+	return choice, memory + 1
+
+'''
+SCORE HISTORY
+2.578
+2.563
+2.502
+2.571
+2.532
+2.590
+'''
