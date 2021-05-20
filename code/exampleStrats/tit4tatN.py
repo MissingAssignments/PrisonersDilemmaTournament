08fd471d24445e305270575a55420841872b71d1
LTA <37591675+LikeToAccess@users.noreply.github.com>
2021-05-20 16:15:41 -0500

    Added comments

--- code/exampleStrats/tit4tatN.py
+++ code/exampleStrats/tit4tatN.py
@@ -3,7 +3,7 @@ def strategy(history, memory):
 		memory = 0
 	choice = 1
 	if (history.shape[1] >= 1 and history[1,-1] == 0) or memory > 2: # Choose to defect if and only if the opponent just defected.
-		memory += 1
-		choice = 0
+		memory += 1  # marks that the other prisoner has defected last turn
+		choice = 0  # Defects to show them a leson!
 
 	return choice, memory
