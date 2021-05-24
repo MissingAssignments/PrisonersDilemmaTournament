b8008841232427c8ea2ce8a9cf10609a80e920af
LikeToAccess <37591675+LikeToAccess@users.noreply.github.com>
2021-05-24 12:08:08 -0500

    Added variation to the files more

--- code/exampleStrats/forgetfulUnforgiving.py
+++ code/exampleStrats/forgetfulUnforgiving.py
@@ -1,13 +1,45 @@
-def strategy(history, memory):
-	if not memory:
-		memory = 1
-	choice = 1
-	# print(memory)
-	if history.shape[1] >= 1 and history[1,-1] == 0 or memory > 3: # Choose to defect if and only if the opponent just defected.
-		choice = 0
-		memory += 1
-	if memory % 10 == 0:
-		choice = 1
-		memory += 1
+# import random
+# import numpy as np
 
-	return choice, memory
+def strategy(history, memory):
+
+	if not memory:
+		# LENGTH_OF_GAME = [int(200-40*np.log(random.random())) for i in range(100)]
+		# total_guesses = 0
+		# for guess in LENGTH_OF_GAME:
+		# 	total_guesses += guess
+		# guess = total_guesses / 100
+		guess = 240
+		memory = [1, 1]
+	else:
+		guess = memory[2]
+	turn = memory[1]
+	choice = 1  # 1:C, 0:D
+	# print(memory)
+	if history.shape[1] >= 1 and history[1,-1] == 0 or memory[0] > 3: # Choose to defect if and only if the opponent just defected.
+		choice = 0
+		memory[0] += 1
+
+	if turn >= guess+60:
+		choice = 0
+
+	if memory[0] % 10 == 0:  # 5
+		choice = 1
+		memory[0] += 1
+
+	return choice, [memory[0], turn+1, guess]
+
+
+#2.723252137519957  | 10
+#2.7245011305028903 | 20
+#2.72745482151492   | 30
+#2.728347344155846  | 50
+#2.730378083992911  | 300
+#2.732313098913462  | N/A
+
+#with guessing
+#2.7172946271143745
+#2.71539567341284
+
+#without guessing
+#2.721954827946978
--- code/prisonersDilemma.py
+++ code/prisonersDilemma.py
@@ -4,9 +4,13 @@ import importlib
 import numpy as np
 import random
 from tqdm import tqdm
+from threading import Thread
+from time import sleep
 
 STRATEGY_FOLDER = "exampleStrats"
 RESULTS_FILE = "results.txt"
+
+done = 0
 
 pointsArray = [[1,5],[0,3]] # The i-j-th element of this array is how many points you receive if you do play i, and your opponent does play j.
 moveLabels = ["D","C"]
@@ -24,120 +28,150 @@ moveLabels = ["D","C"]
 # if there have been 3 turns, and we have defected twice then cooperated once,
 # and our opponent has cooperated all three times.
 def getVisibleHistory(history, player, turn):
-    historySoFar = history[:,:turn].copy()
-    if player == 1:
-        historySoFar = np.flip(historySoFar,0)
-    return historySoFar
+	historySoFar = history[:,:turn].copy()
+	if player == 1:
+		historySoFar = np.flip(historySoFar,0)
+	return historySoFar
 
 def strategyMove(move):
-    if type(move) is str:
-        defects = ["defect","tell truth"]
-        return 0 if (move in defects) else 1
-    else:
-        return move
+	if type(move) is str:
+		defects = ["defect","tell truth"]
+		return 0 if (move in defects) else 1
+	else:
+		return move
 
 def runRound(pair):
-    moduleA = importlib.import_module(STRATEGY_FOLDER+"."+pair[0])
-    moduleB = importlib.import_module(STRATEGY_FOLDER+"."+pair[1])
-    memoryA = None
-    memoryB = None
+	moduleA = importlib.import_module(STRATEGY_FOLDER+"."+pair[0])
+	moduleB = importlib.import_module(STRATEGY_FOLDER+"."+pair[1])
+	memoryA = None
+	memoryB = None
 
-    LENGTH_OF_GAME = int(200-40*np.log(random.random())) # The games are a minimum of 50 turns long. The np.log here guarantees that every turn after the 50th has an equal (low) chance of being the final turn.
-    history = np.zeros((2,LENGTH_OF_GAME),dtype=int)
+	LENGTH_OF_GAME = int(200-40*np.log(random.random())) # The games are a minimum of 50 turns long. The np.log here guarantees that every turn after the 50th has an equal (low) chance of being the final turn.
+	history = np.zeros((2,LENGTH_OF_GAME),dtype=int)
 
-    for turn in range(LENGTH_OF_GAME):
-        playerAmove, memoryA = moduleA.strategy(getVisibleHistory(history,0,turn),memoryA)
-        playerBmove, memoryB = moduleB.strategy(getVisibleHistory(history,1,turn),memoryB)
-        history[0,turn] = strategyMove(playerAmove)
-        history[1,turn] = strategyMove(playerBmove)
+	for turn in range(LENGTH_OF_GAME):
+		playerAmove, memoryA = moduleA.strategy(getVisibleHistory(history,0,turn),memoryA)
+		playerBmove, memoryB = moduleB.strategy(getVisibleHistory(history,1,turn),memoryB)
+		history[0,turn] = strategyMove(playerAmove)
+		history[1,turn] = strategyMove(playerBmove)
 
-    return history
+	return history
 
 def tallyRoundScores(history):
-    scoreA = 0
-    scoreB = 0
-    ROUND_LENGTH = history.shape[1]
-    for turn in range(ROUND_LENGTH):
-        playerAmove = history[0,turn]
-        playerBmove = history[1,turn]
-        scoreA += pointsArray[playerAmove][playerBmove]
-        scoreB += pointsArray[playerBmove][playerAmove]
-    return scoreA/ROUND_LENGTH, scoreB/ROUND_LENGTH
+	scoreA = 0
+	scoreB = 0
+	ROUND_LENGTH = history.shape[1]
+	for turn in range(ROUND_LENGTH):
+		playerAmove = history[0,turn]
+		playerBmove = history[1,turn]
+		scoreA += pointsArray[playerAmove][playerBmove]
+		scoreB += pointsArray[playerBmove][playerAmove]
+	return scoreA/ROUND_LENGTH, scoreB/ROUND_LENGTH
 
 def outputRoundResults(f, pair, roundHistory, scoresA, scoresB):
-    f.write(pair[0]+" (P1)  VS.  "+pair[1]+" (P2)\n")
-    for p in range(2):
-        for t in range(roundHistory.shape[1]):
-            move = roundHistory[p,t]
-            f.write(moveLabels[move]+" ")
-        f.write("\n")
-    f.write("Final score for "+pair[0]+": "+str(scoresA)+"\n")
-    f.write("Final score for "+pair[1]+": "+str(scoresB)+"\n")
-    f.write("\n")
+	f.write(pair[0]+" (P1)  VS.  "+pair[1]+" (P2)\n")
+	for p in range(2):
+		for t in range(roundHistory.shape[1]):
+			move = roundHistory[p,t]
+			f.write(moveLabels[move]+" ")
+		f.write("\n")
+	f.write("Final score for "+pair[0]+": "+str(scoresA)+"\n")
+	f.write("Final score for "+pair[1]+": "+str(scoresB)+"\n")
+	f.write("\n")
 
 def pad(stri, leng):
-    result = stri
-    for i in range(len(stri),leng):
-        result = result+" "
-    return result
+	result = stri
+	for i in range(len(stri),leng):
+		result = result+" "
+	return result
 
 def runFullPairingTournament(inFolder, outFile, silent=False):
-    print("Starting tournament, reading files from "+inFolder)
-    scoreKeeper = {}
-    STRATEGY_LIST = []
-    for file in os.listdir(inFolder):
-        if file.endswith(".py"):
-            STRATEGY_LIST.append(file[:-3])
+	global tracked_strat
+	global done
+	if not silent:
+		print("Starting tournament, reading files from "+inFolder)
+	scoreKeeper = {}
+	STRATEGY_LIST = []
+	for file in os.listdir(inFolder):
+		if file.endswith(".py"):
+			STRATEGY_LIST.append(file[:-3])
 
 
-    for strategy in STRATEGY_LIST:
-        scoreKeeper[strategy] = 0
+	for strategy in STRATEGY_LIST:
+		scoreKeeper[strategy] = 0
 
-    f = open(outFile,"w+")
-    for pair in itertools.combinations(STRATEGY_LIST, r=2):
-        roundHistory = runRound(pair)
-        scoresA, scoresB = tallyRoundScores(roundHistory)
-        outputRoundResults(f, pair, roundHistory, scoresA, scoresB)
-        scoreKeeper[pair[0]] += scoresA
-        scoreKeeper[pair[1]] += scoresB
+	f = open(outFile,"w+")
+	for pair in itertools.combinations(STRATEGY_LIST, r=2):
+		roundHistory = runRound(pair)
+		scoresA, scoresB = tallyRoundScores(roundHistory)
+		outputRoundResults(f, pair, roundHistory, scoresA, scoresB)
+		scoreKeeper[pair[0]] += scoresA
+		scoreKeeper[pair[1]] += scoresB
 
-    scoresNumpy = np.zeros(len(scoreKeeper))
-    for i in range(len(STRATEGY_LIST)):
-        scoresNumpy[i] = scoreKeeper[STRATEGY_LIST[i]]
-    rankings = np.argsort(scoresNumpy)
+	scoresNumpy = np.zeros(len(scoreKeeper))
+	for i in range(len(STRATEGY_LIST)):
+		scoresNumpy[i] = scoreKeeper[STRATEGY_LIST[i]]
+	rankings = np.argsort(scoresNumpy)
 
-    msg = "\nTOTAL SCORES\n"
-    if not silent:
-        print(msg)
-    f.write(msg)
-    for rank in range(len(STRATEGY_LIST)):
-        i = rankings[-1-rank]
-        score = scoresNumpy[i]
-        scorePer = score/(len(STRATEGY_LIST)-1)
-        msg = "#"+str(rank+1)+": "+pad(STRATEGY_LIST[i]+":",16)+' %.3f'%score+'  (%.3f'%scorePer+" average)\n"
-        f.write(msg)
-        if not silent:
-            print(msg, end="")
-        if STRATEGY_LIST[i] in tracked_strat:
-            with open(f"{STRATEGY_LIST[i]}.txt", "a") as file:
-                file.write(f"{scorePer}\n")
-    f.flush()
-    f.close()
-    if not silent:
-        print("\nDone with everything! Results file written to "+RESULTS_FILE)
+	msg = "\nTOTAL SCORES\n"
+	if not silent:
+		print(msg)
+		f.write(msg)
+	for rank in range(len(STRATEGY_LIST)):
+		i = rankings[-1-rank]
+		score = scoresNumpy[i]
+		scorePer = score/(len(STRATEGY_LIST)-1)
+		msg = "#"+str(rank+1)+": "+pad(STRATEGY_LIST[i]+":",16)+' %.3f'%score+'  (%.3f'%scorePer+" average)\n"
+		if not silent:
+			f.write(msg)
+			print(msg, end="")
+		if STRATEGY_LIST[i] in tracked_strat.keys():
+			tracked_strat[STRATEGY_LIST[i]].append(scorePer)
+
+	f.flush()
+	f.close()
+	if not silent:
+		print("\nDone with everything! Results file written to "+RESULTS_FILE)
+	# print("Finished thread.")
+	done += 1
+	# print(done)
 
 
-tracked_strat = ["forgetfulUnforgiving"]
+tracked_strat = {
+	"forgetfulUnforgiving":[],
+	"tit4tat9":[],
+	"tit4tatN":[],
+	"generous4tat2":[]
+}
+
 runFullPairingTournament(STRATEGY_FOLDER, RESULTS_FILE)
 
+iterations = 100
+for i in tqdm(range(iterations)):
+	x = Thread(target=runFullPairingTournament, args=(STRATEGY_FOLDER, RESULTS_FILE, True))
+	x.start()
+print("Waiting for IO operations to finish...")
+# print(tracked_strat)
+memory_done = 1
 
-for i in tqdm(range(100)):
-    runFullPairingTournament(STRATEGY_FOLDER, RESULTS_FILE, silent=True)
-    for strat in tracked_strat:
-        score = 0
-        with open(f"{tracked_strat}.txt", "r") as file:
-            lines = file.read().split("\n")
-        for line in lines:
-            if line:
-                score += int(line)
-        print(f"{strat}: {score}")
+with tqdm(total=iterations) as pbar:
+	while True:
+		# tqdm(done)
+
+		# sleep(1)
+
+		if done == iterations + 1:
+			for strat in tracked_strat:
+				total_score = 0
+				# print(tracked_strat)
+				# print(tracked_strat[strat])
+				for score in tracked_strat[strat]:
+					# print(score)
+					# pass
+					total_score += float(score)
+				print(f"{pad(strat,16)}: {total_score/len(tracked_strat[strat])}")
+			break
+
+		while memory_done < done:
+			pbar.update(1)
+			memory_done += 1
